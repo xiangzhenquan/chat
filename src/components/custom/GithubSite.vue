47dbf401ab007acec8dd388c0e173a2505375cda
xiangzhenquan <45387863+xiangzhenquan@users.noreply.github.com>
2023-04-04 12:52:38 +0800

    Update GithubSite.vue

--- src/components/custom/GithubSite.vue
+++ src/components/custom/GithubSite.vue
@@ -2,7 +2,7 @@
   <div class="text-neutral-400">
     <span>Star on</span>
     <a href="https://github.com/Chanzhaoyu/chatgpt-bot" target="_blank" class="text-blue-500">
-      GitHub
+      chat
     </a>
   </div>
 </template>
